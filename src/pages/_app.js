65b28025cfd5ca168ecf9b9ee91a8f8c4f35bd1f
adilaben <1004nujes@gmail.com>
2023-01-28 22:54:49 +0100

    LoadingBar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,19 @@ import { Layout } from "@/components";
 import { Toaster } from "react-hot-toast";
 import { StateContext } from "@/context/StateContext";
 import "@/styles/globals.css";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#F02D34",
+  className: "z-50",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 export default function App({ Component, pageProps }) {
   return (
