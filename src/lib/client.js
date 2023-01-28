4183379b103af19b9a359392e71fff3bf4f47394
adilaben <1004nujes@gmail.com>
2023-01-28 01:52:45 +0100

    Sanity HookUp and HeroBanner

---
+++ src/lib/client.js
@@ -0,0 +1,14 @@
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = sanityClient({
+  projectId: "yb4wz5p0",
+  dataset: "production",
+  apiVersion: "2023-01-28",
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,33 @@
-import { HeroBanner } from "@/components";
+import { FooterBanner, HeroBanner } from "@/components";
+import { client } from "@/lib/client";
 import React from "react";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speaker of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 1"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
-      Footer
+      <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
